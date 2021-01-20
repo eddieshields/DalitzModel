a3eb19140e7533b5f0846ed9a18b2ab95213cb28
Edward Shields <edward.brendan.shields@cern.ch>
2021-01-20 17:27:55 +0000

    Add dict for root compilation

---
+++ dict/DalitzModelDict.h
@@ -0,0 +1,17 @@
+#ifndef DICT_DALITZMODELDICT_H
+#define DICT_DALITZMODELDICT_H 1
+// Include files
+
+#include "coefficient.h"
+#include "configfile.h"
+#include "configureamplitude.h"
+#include "dalitzamplitude.h"
+#include "dalitzmixing.h"
+#include "parameter.h"
+#include "phasespace.h"
+#include "random.h"
+#include "resonance.h"
+#include "lineshapes/flatte.h"
+#include "lineshapes/relbreitwigner.h"
+
+#endif // DICT_DALITZMODELDICT_H

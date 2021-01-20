cc4b6807e361e5d5a809603484487cb9df3afa91
Edward Shields <edward.brendan.shields@cern.ch>
2021-01-20 17:35:20 +0000

    Fixed cmake build

--- .ci/travis_osx.sh
+++ .ci/travis_osx.sh
@@ -5,7 +5,7 @@ echo "Building under OS: $TRAVIS_OS_NAME"
 mkdir -p build
 cd build
 echo "CMake-ing, CXX = $CXX"
-cmake .. 
+cmake -DBUILD_TESTS=ON -DBUILD_ROOT=OFF .. 
 echo "Building ..."
 make
 cd ..

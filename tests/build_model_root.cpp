7b53418dc8b155f1ca0dc139c40a98dba25a9ad4
Edward Shields <edward.brendan.shields@cern.ch>
2021-01-21 18:12:36 +0000

    debugging model

--- tests/build_model_root.cpp
+++ tests/build_model_root.cpp
@@ -16,7 +16,7 @@ int main()
 {
   DalitzModel::ConfigureAmplitude configure("cfg/babar2008.cfg");
   DalitzModel::Amplitude amp = configure();
-  
+  DalitzModel::LineShape::Flatte::SetParameterisation( "BaBar2008" );
   TH2D* hist = new TH2D("hist","hist",1000,amp.ps().mSq12min(),amp.ps().mSq12max(),1000,amp.ps().mSq13min(),amp.ps().mSq13max());
 
   double mSq12, mSq13;
@@ -24,7 +24,7 @@ int main()
     for (int j = 1; j < hist->GetNbinsY() + 1; j++) {
       mSq12 = hist->GetXaxis()->GetBinCenter(i);
       mSq13 = hist->GetYaxis()->GetBinCenter(j);
-      hist->SetBinContent(i,j,amp.AdirSq(mSq12,mSq13));
+      hist->SetBinContent(i,j,amp.AdirSq("a00_980",mSq12,mSq13));
     }
   }
 
